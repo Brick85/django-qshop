43bbb1855e762a9a7ae41b56d1ab53dd5ab66814
Vital Belikov <vital@qwe.lv>
2013-02-27 14:18:14 +0200

    watermarks

--- qshop/thumbnail_processors.py
+++ qshop/thumbnail_processors.py
@@ -1,10 +1,13 @@
 from PIL import Image
+from django.conf import settings
 from django.contrib.staticfiles import finders
 
-def watermark_processor(image, watermark=False, **kwargs):
-    if watermark:
-        if not hasattr(watermark_processor, '_watermark_image'):
-            watermark_processor._watermark_image = Image.open(finders.find(watermark))
-        watermark_image = watermark_processor._watermark_image
+def watermark_processor(image, watermark=None, **kwargs):
+    if watermark!=None:
+        if not hasattr(watermark_processor, '_watermarks'):
+            watermark_processor._watermarks = {}
+        if not watermark in watermark_processor._watermarks:
+            watermark_processor._watermarks[watermark] = Image.open(finders.find(settings.WATERMARKS[watermark]))
+        watermark_image = watermark_processor._watermarks[watermark]
         image.paste(watermark_image, ((image.size[0]-watermark_image.size[0]), (image.size[1]-watermark_image.size[1])), watermark_image)
     return image
