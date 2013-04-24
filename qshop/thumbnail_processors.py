43b899fe5acb0f3ca01d5234a25ca97f52acc654
Vital Belikov <vital@qwe.lv>
2013-04-24 17:38:30 +0300

    wm

--- qshop/thumbnail_processors.py
+++ qshop/thumbnail_processors.py
@@ -2,13 +2,14 @@ from PIL import Image
 from django.conf import settings
 from django.contrib.staticfiles import finders
 
+
 def watermark_processor(image, wm=None, wmp="bl", **kwargs):
-    if watermark!=None:
+    if wm is not None:
         if not hasattr(watermark_processor, '_watermarks'):
             watermark_processor._watermarks = {}
-        if not watermark in watermark_processor._watermarks:
-            watermark_processor._watermarks[watermark] = Image.open(finders.find(settings.WATERMARKS[watermark]))
-        watermark_image = watermark_processor._watermarks[watermark]
+        if not wm in watermark_processor._watermarks:
+            watermark_processor._watermarks[wm] = Image.open(finders.find(settings.WATERMARKS[wm]))
+        watermark_image = watermark_processor._watermarks[wm]
         if wmp == "bl":
             image.paste(watermark_image, ((image.size[0]-watermark_image.size[0]), (image.size[1]-watermark_image.size[1])), watermark_image)
         else:
