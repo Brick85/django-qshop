b7736243850cf768dde48c5c2e35f69f272b12d6
Vital Belikov <vital@qwe.lv>
2014-09-22 12:18:05 +0300

    product links

--- qshop/urls.py
+++ qshop/urls.py
@@ -3,9 +3,9 @@ from .views import redirect_to_product, set_currency
 import qshop_settings
 
 urlpatterns = patterns('',
-    url(r'^show_product/(?P<product_id>\d+)/$', redirect_to_product, name='redirect_to_product'),
-    url(r'^shop_set_currency/$', set_currency, name='set_currency'),
-    url(r'^shop_set_currency/(?P<currency_code>[\w]+)/$', set_currency, name='set_currency'),
+    url(r'^show-product/(?P<product_id>\d+)/$', redirect_to_product, name='redirect_to_product'),
+    url(r'^shop-set-currency/$', set_currency, name='set_currency'),
+    url(r'^shop-set-currency/(?P<currency_code>[\w]+)/$', set_currency, name='set_currency'),
 )
 
 if qshop_settings.ENABLE_PAYMENTS:
