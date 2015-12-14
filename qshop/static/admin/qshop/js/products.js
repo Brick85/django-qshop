f28f4240111af83a83dd84293a53fa9437f3e181
Alexey <a@trialine.lv>
2015-12-14 17:32:17 +0200

    django 1.8 in product edit, product to parameter section bugfix

--- qshop/static/admin/qshop/js/products.js
+++ qshop/static/admin/qshop/js/products.js
@@ -5,10 +5,20 @@
             select.hide();
             $(select).after('<p>' + $('option:selected', select).html() + '</p>');
         });
-        $('#producttoparameter_set-group a.add-another').each(function(){
+
+        $('#producttoparameter_set-group a.add-related').each(function(){
             id = $('.field-parameter select', $(this).closest('tr')).val();
             this.href += (this.href.indexOf('?')!= -1 ? '&' : '?') + 'parameter=' + id + '&hide_parameter';
         });
+
+        $('#producttoparameter_set-group a.change-related').each(function(){
+            id = $('.field-parameter select', $(this).closest('tr')).val();
+            href_template = $(this).data('href-template');
+            $(this).attr('data-href-template', href_template + (href_template.indexOf('?')!= -1 ? '&' : '?') + 'parameter=' + id + '&hide_parameter');
+
+        });
+
+
     });
 
 })(django.jQuery);
