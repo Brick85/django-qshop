6e5f0a894e8fee15b5f3af47b91a4e166a2ab178
Alexey <a@trialine.lv>
2016-02-25 20:42:54 +0200

    urls.py deprecation warning

--- qshop/urls.py
+++ qshop/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls import patterns, url, include
 from .views import redirect_to_product, set_currency
 import qshop_settings
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^show-product/(?P<product_id>\d+)/$', redirect_to_product, name='redirect_to_product'),
     url(r'^shop-set-currency/$', set_currency, name='set_currency'),
     url(r'^shop-set-currency/(?P<currency_code>[\w]+)/$', set_currency, name='set_currency'),
-)
+]
 
 if qshop_settings.ENABLE_PAYMENTS:
-    urlpatterns += patterns('',
+    urlpatterns += [
         url(r'^vendors/', include('qshop.payment_vendors.urls')),
-    )
+    ]
