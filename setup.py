80959f223f1c93f9d84bbc6ab80f42ed44799e83
Vitālijs Gaičuks <vitalijs.gaicuks@gmail.com>
2019-02-08 16:24:57 +0200

    Previous commit raised bug on 'save as new'. There have to be p2p.parameter ids on form. Therefore to avoid using select wigdet, is hidden field with readonly paramenter name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='django-qshop',
-      version='0.4.0',
+      version='0.4.1',
       description='E-commerce for django. Requires django-sitemenu.',
       long_description='E-commerce for django. Requires django-sitemenu.',
       author='Vital Belikov',
