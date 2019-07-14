560d874286c79b0a101a8972380c80e900e6fa8b
Alexey <a@trialine.lv>
2019-07-14 18:52:48 +0300

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='django-qshop',
-      version='0.4',
+      version='0.4.1',
       description='E-commerce for django. Requires django-sitemenu. Beta version.',
       long_description='E-commerce for django. Requires django-sitemenu. Beta version.',
       author='Vital Belikov',
