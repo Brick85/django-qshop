7d9cd562e351aacca1b48fe163adf1cbf8234920
Alexey <a@trialine.lv>
2021-04-08 19:00:54 +0300

    translations

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='django-qshop',
-      version='1.2.3',
+      version='1.2.4',
       description='E-commerce for django. Requires django-sitemenu. Beta version.',
       long_description='E-commerce for django. Requires django-sitemenu. Beta version.',
       author='Vital Belikov',
