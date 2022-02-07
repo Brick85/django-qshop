e1167cc7adfde2ab3c6e6c757323a326c8c78e6d
Alexey <a@trialine.lv>
2022-02-07 12:38:25 +0200

    version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='django-qshop',
-      version='1.2.14',
+      version='1.2.15',
       description='E-commerce for django. Requires django-sitemenu. Beta version.',
       long_description='E-commerce for django. Requires django-sitemenu. Beta version.',
       author='Vital Belikov',
